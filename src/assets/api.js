591a125d553f3d70e8e918f63ab8f14165439b41
Devjiro76 <devjiro76@gmail.com>
2018-06-29 15:57:13 +0900

    그냥... 커밋해봤어

--- src/assets/api.js
+++
@@ -1,3 +0,0 @@
-{
-  "result": "This is Test Result. {{ul01}}"
-}
--- src/index.js
+++ src/index.js
@@ -1,54 +1,26 @@
 var app = document.body.querySelector("#app");
-var lang = localStorage.getItem('lang') || 'en-us';
-var dictionary = {};
+var data = {
+  dictionary: null,
+  lang: localStorage.getItem('lang') || 'en-us'
+};
 
-init(lang);
+function getLang() {
+  return data.lang;
+}
 
-function init(_lang) {
-  if (_lang) {
-    localStorage.setItem('lang', _lang);
-    lang = localStorage.getItem('lang') || 'en-us';
+function setLang(lang) {
+  if (lang) {
+    localStorage.setItem('lang', lang);
+    data.lang = localStorage.getItem('lang') || 'en-us';
   }
+}
 
+function loadPage(page) {
   app.innerHTML = "";
 
-  Promise.resolve()
-    .then(function() {
-      console.log("dictionary loading...");
-      return new Promise(function (resolve) {
-        var q = [];
-
-        q.push(load('./assets/lang/en-us.json'));
-        q.push(load('./assets/lang/ko-kr.json'));
-
-        Promise.all(q)
-          .then(function (res) {
-            res.forEach(function (_item) {
-              var item = JSON.parse(_item);
-              dictionary[item.lang] = item.contents;
-            });
-            setTimeout(function() {
-              resolve();
-            }, 0);
-          });
-      });
-    })
-    .then(function () {
-      console.log("dictionary loaded.");
-      console.log('translator is loading...');
-      return load('./trans.js', {
-        translate: false
-      });
-    })
-    .then(function () {
-      console.log('translator is loaded.');
-      return load('./template/app.html');
-    })
-    .then(function () {
-      console.log('assets(css, js) are loading.');
-      load('./template/app.css');
-      load('./template/app.js');
-    });
-  
-  
+  load("./template/" +page+ ".css");
+  load("./template/" +page+ ".html");
+  load("./template/" +page+ ".js");
 }
+
+loadPage("temp1");
